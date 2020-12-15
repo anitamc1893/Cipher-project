75979b52b610e79455526021e476301d437c4510
anitamc1893 <anitamc1893@gmail.com>
2020-12-14 20:39:22 -0600

    cifrado en consola

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,30 @@
-const cipher = {
+const cipher = {//esto es un objeto
   // ...
+  encode:function(offset, text){//pasar los parametros
+
+    let resultado;
+    let array=[]
+
+    for(let i=0; i<text.length; i++){ //iterador
+        //let ascii=text.charCodeAt(i);
+        
+        if (text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90) {
+               resultado = String.fromCharCode((text.charCodeAt(i) - 65 + offset) %26 + 65);
+                  array.push(resultado)  }
+                  else if (text.charCodeAt(i) >= 97 && text.charCodeAt(i) <= 122) {
+                    resultado = String.fromCharCode((text.charCodeAt(i) - 97 + offset) % 26 + 97);
+                    array.push(resultado)
+                    console.log(array)
+                  }   
+    }
+    
+    return array.join("") //retornar el texto codificado
+},
+
+
+decode:function(){
+  return "esta funcion decodifica"
+}
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import cipher from './cipher.js';
+import cipher from './cipher.js'
+    let offset = document.getElementById("recorrido").value;
+    let text = document.getElementById("texto").value;
+    
+console.log(cipher.encode(1,"ana"))
 
-console.log(cipher);
+
+//LA siguiente linea accede al dom
+let elespejo=document.querySelector(".espejo")
+//La siguiente linea manupular el dom
+elespejo.innerHTML="<marquee>Hola Soy un espejo</marquee>"
