52acecbdb52fe1d60009c2953d76636d23897104
anitamc1893 <anitamc1893@gmail.com>
2020-12-20 23:34:37 -0600

    Cifrado, Descrifrado, CSS completo, test 74%

--- src/cipher.js
+++ src/cipher.js
@@ -1,30 +1,74 @@
-const cipher = {//esto es un objeto
+const cipher = {
+  //esto es un objeto
   // ...
-  encode:function(offset, text){//pasar los parametros
-
+  encode: function (offset, text) {
+    //pasar los parametros
     let resultado;
-    let array=[]
-
-    for(let i=0; i<text.length; i++){ //iterador
-        //let ascii=text.charCodeAt(i);
-        
-        if (text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90) {
-               resultado = String.fromCharCode((text.charCodeAt(i) - 65 + offset) %26 + 65);
-                  array.push(resultado)  }
-                  else if (text.charCodeAt(i) >= 97 && text.charCodeAt(i) <= 122) {
-                    resultado = String.fromCharCode((text.charCodeAt(i) - 97 + offset) % 26 + 97);
-                    array.push(resultado)
-                    console.log(array)
-                  }   
-    }
+    let array = [];
     
-    return array.join(" ") //retornar el texto codificado
-},
+
+    if (offset == null){
+      throw new TypeError ("Not valid");
+    }else if(text == []){
+      throw new TypeError("Not valid");
+    }else if( offset == 0){
+      throw new TypeError ("Not valid");
+    }
+
+    for (let i = 0; i < text.length; i++) {
+      //iterador
+      //let ascii=text.charCodeAt(i);
+      const charValue = text.charCodeAt(i);
+
+      if (charValue >= 65 && charValue <= 90) {
+        resultado = String.fromCharCode(((charValue - 65 + offset) % 26) + 65);
+        array.push(resultado);
+      } else if (charValue >= 97 && charValue <= 122) {
+        resultado = String.fromCharCode(((charValue - 97 + offset) % 26) + 97);
+        array.push(resultado);
+      } else {
+        resultado = String.fromCharCode(charValue);
+        array.push(resultado);
+      }
+    }
+
+    return array.join(""); //retornar el texto codificado
+  },
+
+  decode: function (offset2, text2) {
+    let resultado;
+    let array = [];
+
+    if (offset2 == null){
+      throw new TypeError ("Not valid");
+    }else if(text2 == []){
+      throw new TypeError("Not valid");
+    }else if( offset2 == 0){
+      throw new TypeError ("Not valid");
+    }
+
+    for (let i = 0; i < text2.length; i++) {
+      //iterador
+      //let ascii=text.charCodeAt(i);
+      const charValue2 = text2.charCodeAt(i);
 
 
-decode:function(){
-  return "esta funcion decodifica"
-}
+      if (charValue2 >= 65 && charValue2 <= 90) {
+        resultado = String.fromCharCode(((charValue2 - 65 - offset2 + 26) % 26) + 65);
+        array.push(resultado);
+      } else if (charValue2 >= 97 && charValue2 <= 122) {
+        resultado = String.fromCharCode(
+          ((charValue2 - 97 - offset2 + 26) % 26) + 97
+        );
+        array.push(resultado);
+         } else {
+        resultado = String.fromCharCode(charValue2);
+        array.push(resultado);
+      }
+    }
+
+    return array.join(""); //retornar el texto codificado
+  },
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import cipher from './cipher.js'
-    let offset = document.getElementById("recorrido").value;
-    let text = document.getElementById("texto").value;
-    
-console.log(cipher.encode(1,"ana"))
+import cipher from "./cipher.js";
+let offset = document.getElementById("recorrido");
+let text = document.getElementById("texto");
+let botonEncode = document.getElementById("boton");
 
+botonEncode.addEventListener("click", function (e) {
+  e.preventDefault();
+  let resultado = cipher.encode(parseInt(offset.value), text.value);
+  //document.write(resultado);
+  document.getElementById("textCifrado").innerHTML = resultado;
+});
 
-//LA siguiente linea accede al dom
-let elespejo=document.querySelector(".espejo")
-//La siguiente linea manupular el dom
-elespejo.innerHTML="<marquee>Hola Soy un espejo</marquee>"
+let offset2 = document.getElementById("recorrido2");
+let text2 = document.getElementById("texto2");
+let botonDecode = document.getElementById("boton2");
+
+botonDecode.addEventListener("click", function (e) {
+  e.preventDefault();
+  let resultado = cipher.decode(parseInt(offset2.value), text2.value);
+  document.getElementById("textDescifrado").innerHTML = resultado;
+});
+
